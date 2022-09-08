a8a051e3b793cdfc5d793e9fba8793f146ab0490
Peter Ndumia <ndegwandumia@gmail.com>
2022-09-08 21:51:22 +0300

    Added post methods

--- jobo-server/app/controllers/application_controller.rb
+++ jobo-server/app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < Sinatra::Base
   end
   get "/companies" do
     companies = Company.all
-    companies.to_json
+    companies.to_json(include: :categories)
     
   end
   get "/categories" do
@@ -20,6 +20,37 @@ class ApplicationController < Sinatra::Base
     jobs = Job.all
     jobs.to_json
     
+  end
+  get "/companies/:id" do
+    company = Company.find(params[:id])
+    company.to_json
+  end
+
+  get "/categories/:id" do
+    category = Category.find(params[:id])
+    category.to_json
+  end
+  get "/jobs/:id" do
+    job = Job.find(params[:id])
+    job.to_json
+  end
+  post "/companies" do 
+    company = Company.create(company_name: params[:company_name],
+    company_desc: params[:company_desc])
+    company.to_json
+  end
+  post "/categories" do 
+    category = Category.create(category_name: params[:category_name],
+    category_desc: params[:category_desc])
+    category.to_json
+  end
+  post "/jobs" do 
+    jobs = Job.create(job_title: params[:job_title ],
+    job_desc: params[:job_desc], 
+    how_to_apply: params[:how_to_apply], 
+    company_id: params[:company_id], 
+    category_id: params[category_id])
+    category.to_json
   end
 
 
--- jobo-server/db/seeds.rb
+++ jobo-server/db/seeds.rb
@@ -1,11 +1,15 @@
 puts "🌱 Seeding spices..."
 
 # Seed your database here
-
 apple = Company.create(company_name: "Apple inc.", company_desc: "Apple company deals in IT")
+microsoft= Company.create(company_name: "Microsoft inc.", company_desc: "Microsoft company deals in IT")
 
 software_engineering = Category.create(category_name: "Software Engineering", category_desc: "Jobs available in IT")
+android_engineering = Category.create(category_name: "Android Engineering", category_desc: "Jobs available in IT")
 
 software_engineer = Job.create(job_title: "Software Engineer", job_desc: "Work as a software engineer", how_to_apply: "Send your resume to apple@recruitment.inc", company_id:1, category_id:1)
+android_engineer = Job.create(job_title: "Android Engineer", job_desc: "Work as a android engineer", how_to_apply: "Send your resume to microsoft@recruitment.inc", company_id:2, category_id:2)
+
+
 
 puts "✅ Done seeding!"
