5c9b0ed01ff0769e1252ee154ab1080ca8da0681
Peter Ndumia <ndegwandumia@gmail.com>
2022-09-09 11:10:10 +0300

    Added background image

--- jobo-client/src/components/App.js
+++ jobo-client/src/components/App.js
@@ -6,7 +6,6 @@ import { Route, Routes } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-
         <NavBar/>
         <Routes>
             <Route path="/" element= {<Home/>}></Route>
--- jobo-client/src/components/Home.js
+++ jobo-client/src/components/Home.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function Home(props) {
     return (
-        <div>
-
-            <p className="welcome">  Hello  world!</p>
+        <div className="home-container">
+            <button></button>
+            <button></button> 
         </div>
     );
 }
